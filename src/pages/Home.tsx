6d03f3370e382c35ea81b9fa66186126e07c26d5
Rumsha Siddiqui <rumsha7@gmail.com>
2018-08-09 15:47:32 -0700

    lint update and toggle working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,35 +8,54 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoStream: null
+      videoStream: null,
+      front: false
     }
   }
 
   componentDidMount() {
     console.log("ComponentDidMount");
+    this.handleCamera();
+  }
+
+  handleCamera() {
     //Set facingMode to "environment" for rear camera and "user" for front camera
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false })
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: (((this.state as any).front ? "user" : "environment")) }, audio: false })
       .then((stream) => {
+        console.log('handleCamera');
+        console.log((this.state as any).front);
         this.setState({
           videoStream: stream
         });
 
         const correctVideoElement = this.mainVideo.current!;
 
-        if (this.mainVideo) {
+        if (this.mainVideo && correctVideoElement) {
           correctVideoElement.srcObject = (this.state as any).videoStream;
+          console.log(stream.getVideoTracks()[0].getConstraints().facingMode);
         }
+        
       })
       .catch((err) => {
         console.error(err);
       });
   }
 
+  handleCameraToggle() {
+    console.log('handle camera toggle'); 
+   (this.state as any).videoStream.getTracks()[0].stop();
+   this.setState({
+      front: !(this.state as any).front
+    }, () =>{
+      this.handleCamera();
+    });
+  }
+
   render() {
     return (
       <div className="Home">
         <video autoPlay={true} ref={this.mainVideo} className="Home-Video"></video>
-        <button>Front</button><button>Rear</button>
+        <button onClick={() => this.handleCameraToggle()}>Toggle</button>
       </div>
     );
   }
