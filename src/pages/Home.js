f13b830cd7e629a7e0adb3995b76a371699cb21b
Justin Willis <jgw9617@gmail.com>
2018-08-09 13:13:45 -0700

    feat(): getting video access

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            videoStream: null
-        }
-        this.mainVideo = React.createRef();
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoStream: null
     }
+    this.mainVideo = React.createRef();
+  }
 
-    componentDidMount() {
-        console.log("ComponentDidMount");
-        navigator.mediaDevices.getUserMedia({video: true, audio: false})
-            .then((stream) => {
-                /* use the stream */
-                console.log(stream);
-                //var video = document.querySelector('video');
-                //this.state.videoStream = stream;
-                //this.state.videoStream.onloadedmetadata = function(e) {
-                  //  this.state.videoStream.play();
-                //};
+  componentDidMount() {
+    console.log("ComponentDidMount");
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        /* use the stream */
+        console.log(stream);
+        //var video = document.querySelector('video');
+        //this.state.videoStream = stream;
+        //this.state.videoStream.onloadedmetadata = function(e) {
+        //  this.state.videoStream.play();
+        //};
 
-                this.setState({
-                    videoStream: stream
-                });
-                this.mainVideo.current.srcObject = this.state.videoStream;
-                this.mainVideo.current.srcObject.onloadedmetadata = function(e) {
-                    console.log("hello");
-                    this.mainVideo.current.srcObject.play();
-                }
-            })
-            .catch(function (err) {
-                /* handle the error */
-            });
-    }
+        this.setState({
+          videoStream: stream
+        });
+        this.mainVideo.current.srcObject = this.state.videoStream;
+      })
+      .catch(function (err) {
+        /* handle the error */
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                <video ref={this.mainVideo}> 
-
-                </video>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <video autoPlay ref={this.mainVideo}></video>
+      </div>
+    );
+  }
 }
 
 export default Home;
