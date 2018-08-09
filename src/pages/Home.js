a8bb164b4ee4e0a264e401b0cdc1af04669a1846
Rumsha Siddiqui <rumsha7@gmail.com>
2018-08-09 13:06:28 -0700

    Trying to get camera to work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,47 @@ import React, { Component } from 'react';
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            videoStream: null
+        }
+        this.mainVideo = React.createRef();
     }
-    render() { 
-        return ( 
+
+    componentDidMount() {
+        console.log("ComponentDidMount");
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then((stream) => {
+                /* use the stream */
+                console.log(stream);
+                //var video = document.querySelector('video');
+                //this.state.videoStream = stream;
+                //this.state.videoStream.onloadedmetadata = function(e) {
+                  //  this.state.videoStream.play();
+                //};
+
+                this.setState({
+                    videoStream: stream
+                });
+                this.mainVideo.current.srcObject = this.state.videoStream;
+                this.mainVideo.current.srcObject.onloadedmetadata = function(e) {
+                    console.log("hello");
+                    this.mainVideo.current.srcObject.play();
+                }
+            })
+            .catch(function (err) {
+                /* handle the error */
+            });
+    }
+
+    render() {
+        return (
             <div>
-                <h1>
-                    Test
-                </h1>
+                <video ref={this.mainVideo}> 
+
+                </video>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Home;
